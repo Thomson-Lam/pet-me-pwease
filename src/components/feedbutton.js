b97eb06faac76b68f4560a37580682ad221ab53a
twang849 <163226113+twang849@users.noreply.github.com>
2024-10-14 21:23:15 -0400

    Added feedbutton img

--- src/components/feedbutton.js
+++ src/components/feedbutton.js
@@ -12,7 +12,7 @@ const FeedButton = () => {
     return (
         <div>
             <button onClick={handleFeed}>
-                <b>Feed pet</b>
+                <img src='https://media.istockphoto.com/id/901501348/vector/slice-of-melted-cheese-pepperoni-pizza.jpg?s=612x612&w=0&k=20&c=0cIsVfLHiabGD1NGDZEfE1hIBmD5DQuzNuLK8Owvyo8=' alt='Feed img' className='pizza'/>
             </button>
             <br />
             <br />
